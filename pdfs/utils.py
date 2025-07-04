0444b2c4869c2615ae5b70c07afd4a1ccb87ce5d
MetalFKingRawner <150486340+MetalFKingRawner@users.noreply.github.com>
2025-07-04 14:46:40 -0600

    Se agregaron loggins
    Para el monitoreo de PDF´s

--- pdfs/utils.py
+++ pdfs/utils.py
@@ -3,8 +3,7 @@ from docx import Document
 from docx2pdf import convert
 from docxtpl import DocxTemplate
 import os
-import subprocess
-import shlex
+import subprocess, shlex, logging
 
 def fill_word_template(template_path, context, output_docx_path):
     """
@@ -21,11 +20,14 @@ def convert_docx_to_pdf(docx_path: str, output_pdf_path: str) -> bool:
     Convierte DOCX a PDF usando unoconv (LibreOffice).
     Lanza RuntimeError si falla.
     """
-    # Construye el comando
     cmd = f'unoconv -f pdf -o "{output_pdf_path}" "{docx_path}"'
+    logger.debug(f"Ejecutando comando: {cmd}")
     proc = subprocess.run(shlex.split(cmd), capture_output=True, text=True)
+    logger.debug(f"unoconv returncode={proc.returncode}")
+    logger.debug(f"unoconv stdout:\n{proc.stdout}")
+    logger.debug(f"unoconv stderr:\n{proc.stderr}")
     if proc.returncode != 0:
-        raise RuntimeError(
-            f"Error en unoconv:\nSTDOUT:{proc.stdout}\nSTDERR:{proc.stderr}"
-        )
+        msg = f"Error en conversión DOCX→PDF (returncode {proc.returncode})."
+        logger.error(msg)
+        raise RuntimeError(f"{msg}\nSTDOUT: {proc.stdout}\nSTDERR: {proc.stderr}")
     return True
