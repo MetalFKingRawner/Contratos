a9b052be0666b5f26f6a4793a2d626221f3a032e
MetalFKingRawner <150486340+MetalFKingRawner@users.noreply.github.com>
2025-08-29 10:17:07 -0600

    views para login

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+# core/views.py
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import StaffAuthenticationForm  # ruta según donde lo colocaste
+from django.urls import reverse_lazy
+
+class AdminLoginView(LoginView):
+    template_name = "accounts/login.html"
+    authentication_form = StaffAuthenticationForm
+    redirect_authenticated_user = True
+    # Si quieres que tras login vaya a dashboard:
+    next_page = reverse_lazy('inicio')  # opcional, LoginView usa setting LOGIN_REDIRECT_URL
+
+class AdminLogoutView(LogoutView):
+    next_page = reverse_lazy('inicio')  # o '/' o el nombre de la vista de inicio
