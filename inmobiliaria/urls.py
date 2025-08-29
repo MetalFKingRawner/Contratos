28c5de7e0aa0097e011e39d5ba66a6d15586548a
MetalFKingRawner <150486340+MetalFKingRawner@users.noreply.github.com>
2025-08-29 10:18:46 -0600

    urls para login

--- inmobiliaria/urls.py
+++ inmobiliaria/urls.py
@@ -7,9 +7,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('accounts/', include('core.urls', namespace='core')),
     path("", wf_views.SeleccionDocumentoView.as_view(), name="inicio"),
     path("workflow/", include("workflow.urls")),
     path("reviews/", include("reviews.urls")),
     path('financiamiento/', include('financiamiento.urls', namespace='financiamiento')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
