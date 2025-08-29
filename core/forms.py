6e7235fa964affd9d43cca4de707487f5f52ee85
MetalFKingRawner <150486340+MetalFKingRawner@users.noreply.github.com>
2025-08-29 10:16:09 -0600

    form login

--- core/forms.py
+++ core/forms.py
@@ -2,6 +2,7 @@
 
 from django import forms
 from .models import Cliente, Vendedor
+from django.contrib.auth.forms import AuthenticationForm
 
 class ClienteForm(forms.ModelForm):
     class Meta:
@@ -43,3 +44,29 @@ class VendedorForm(forms.ModelForm):
         widgets = {
             'domicilio': forms.Textarea(attrs={'rows':2}),
         }
+
+class StaffAuthenticationForm(AuthenticationForm):
+    """
+    Extiende el formulario de autenticación para:
+    - Rechazar usuarios que no sean staff (is_staff).
+    - Añadir atributos de widget (clase CSS) a los campos.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Añadimos clases y placeholders a los widgets
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Usuario'
+        })
+        self.fields['password'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Contraseña'
+        })
+
+    def confirm_login_allowed(self, user):
+        # Reusa la validación del parent para is_active etc. (AuthenticationForm ya maneja is_active)
+        if not (user.is_active and user.is_staff):
+            raise forms.ValidationError(
+                "Acceso denegado: debes ser usuario del staff para iniciar sesión aquí.",
+                code='invalid_login',
+            )
